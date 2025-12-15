5ae171069031293e8e37524282ebd9f0ad33d5b9
Omar <omarherrera_07@icloud.com>
2025-12-15 16:58:51 -0600

    removed port from url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = (import.meta.env.VITE_API_URL) ? import.meta.env.VITE_API_URL : 'http://localhost:4000/api';
+const API = (import.meta.env.VITE_API_URL) ? import.meta.env.VITE_API_URL : 'https://backendhdh.onrender.com';
 
 
 export async function login(username, password){
